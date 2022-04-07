2ddb1a28659420e8490c8841a40a301a7dbe7f58
antonydev <amonteza@ceslysoft.com>
2022-04-07 18:33:42 -0500

    Componente para buscar por país, servicio para buscar países y manejo de errores.

---
+++ src/app/pais/services/pais.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaisService {
+
+  private apiURL: string = 'https://restcountries.com/v3.1';
+
+  constructor( private http: HttpClient) { }
+
+  buscarPais( termino: string ): Observable<any>{
+
+    const url = `${ this.apiURL }/name/${ termino }`;
+    
+    return this.http.get( url );
+    // .pipe(
+    //   catchError( err => of([]))
+    //   ); OBTENER EL ERROR Y RETORNAR UN ARREGLO VACIO
+
+  }
+
+}
